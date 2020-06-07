04cea1732ef27f47705c3c178c3f6d6cb9d62d9c
Rajat Shrivastava <51124175+rajathandsom@users.noreply.github.com>
2020-06-07 23:15:19 +0530

    true post add

--- static/scripts/serviceworker.js
+++ static/scripts/serviceworker.js
@@ -1,4 +1,40 @@
 
+// importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
+
+//    var firebaseConfig = {
+//     apiKey: "AIzaSyBp17YUm4uju7nqT5syTdGIUc_mJ253PH0",
+//     authDomain: "agyavart-27f8b.firebaseapp.com",
+//     databaseURL: "https://agyavart-27f8b.firebaseio.com",
+//     projectId: "agyavart-27f8b",
+//     storageBucket: "agyavart-27f8b.appspot.com",
+//     messagingSenderId: "223304863434",
+//     appId: "1:223304863434:web:585b9e9ace6b42ad51ee33",
+//     measurementId: "G-7KW9B9M084"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   const messaging = firebase.messaging();
+
+// messaging.usePublicVapidKey("BLVRpuCoOigGCotWJDI-3uQNNHVn-HOGxh9TSB0zgRriUZFDiAlrDWxVsqrcHpUhva7Aj20KsbP1vzSV9QU2Cps");
+  
+
+
+
+//   messaging.setBackgroundMessageHandler(function(payload) {
+//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//   // Customize notification here
+//   const notificationTitle = 'Background Message Title';
+//   const notificationOptions = {
+//     body: 'Background Message body.',
+//     icon: '/firebase-logo.png'
+//   };
+
+//   return self.registration.showNotification(notificationTitle,
+//     notificationOptions);
+// });
+
+// The rest of your Service Worker code goes here ...
 /*
 Copyright 2015, 2019 Google Inc. All Rights Reserved.
  Licensed under the Apache License, Version 2.0 (the "License");
