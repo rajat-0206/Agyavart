db1c723f9884b4661ecdb76f19ba7343d3966393
= <=>
2020-06-28 15:16:29 +0530

    search added

--- static/scripts/serviceworker.js
+++ static/scripts/serviceworker.js
@@ -1,73 +1,21 @@
-
-// importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
-
-//    var firebaseConfig = {
-//     apiKey: "AIzaSyBp17YUm4uju7nqT5syTdGIUc_mJ253PH0",
-//     authDomain: "agyavart-27f8b.firebaseapp.com",
-//     databaseURL: "https://agyavart-27f8b.firebaseio.com",
-//     projectId: "agyavart-27f8b",
-//     storageBucket: "agyavart-27f8b.appspot.com",
-//     messagingSenderId: "223304863434",
-//     appId: "1:223304863434:web:585b9e9ace6b42ad51ee33",
-//     measurementId: "G-7KW9B9M084"
-//   };
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-//   const messaging = firebase.messaging();
-
-// messaging.usePublicVapidKey("BLVRpuCoOigGCotWJDI-3uQNNHVn-HOGxh9TSB0zgRriUZFDiAlrDWxVsqrcHpUhva7Aj20KsbP1vzSV9QU2Cps");
-  
-
-
-
-//   messaging.setBackgroundMessageHandler(function(payload) {
-//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-//   // Customize notification here
-//   const notificationTitle = 'Background Message Title';
-//   const notificationOptions = {
-//     body: 'Background Message body.',
-//     icon: '/firebase-logo.png'
-//   };
-
-//   return self.registration.showNotification(notificationTitle,
-//     notificationOptions);
-// });
-
-// The rest of your Service Worker code goes here ...
-/*
-Copyright 2015, 2019 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = 'offline';
-// Customize this with a different URL if needed.
-const OFFLINE_URL = '/offline.html';
+var CACHE_NAME = 'agyavart-cache-v2';
+var urlsToCache = [
+  '/loader.html',
+  '/offline.html',
+  '/static/images/rmailLogoConcept.svg'
+];
 
 self.addEventListener('install', function(event) {
+  // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(
-        [
-          '/loader.html',
-          '/404.html',
-          '/offline.html',
-        ]
-      );
-    })
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
+
+
 
 self.addEventListener('activate', (event) => {
   event.waitUntil((async () => {
@@ -82,22 +30,29 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    // Try the cache
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request).then(function(response) {
-        if (response.status === 404) {
-          return caches.match('/404.html');
+self.addEventListener('fetch', (event) => {
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        // First, try to use the navigation preload response if it's supported.
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
         }
-        return response
-      });
-    }).catch(function() {
-      // If both fail, show a generic fallback:
-      return caches.match('/offline.html');
-    })
-  );
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match('/offline.html');
+        return cachedResponse;
+      }
+    })());
+  }
+
 });
